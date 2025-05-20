887d57bfc513a3c70fcd619487b7a358866058aa
Valay Panchal <valay.pan93@gmail.com>
2025-05-20 14:07:33 +0530

    modified with word tokenization

--- tokenization_example.py
+++ tokenization_example.py
@@ -3,6 +3,7 @@ print("Downloading punkt...")
 nltk.download('punkt')
 
 from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize 
 
 corpus = """Hello , My name is Valay Panchal and welcome to my NLP tutorial.
 please watch my full tutorial! to get expert in NLP
@@ -10,3 +11,6 @@ please watch my full tutorial! to get expert in NLP
 
 print("Tokenized Sentences:")
 print(sent_tokenize(corpus))
+
+print("tokenize word:")
+print(word_tokenize(corpus))
