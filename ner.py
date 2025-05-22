405312a0c1884d1599b035b48a641b51d5a55615
Valay Panchal <valay.pan93@gmail.com>
2025-05-22 14:23:35 +0530

    named entity recognization

---
+++ ner.py
@@ -0,0 +1,19 @@
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+from nltk.tokenize import word_tokenize
+sentence="""The Eiffel Tower was built from 1887 to 1889 by French engineer Gustave Eiffel, whose company specialized in building metal frameworks and structures."
+"""
+# tokenize word
+words = word_tokenize(sentence)
+print('Token:',words)
+
+#part of speech tagging
+tag_elements = nltk.pos_tag(words)
+print('POS tag is:',tag_elements)
+
+#Named entity recognization
+chunk = named_entity= nltk.ne_chunk(tag_elements)
+chunk.draw()
