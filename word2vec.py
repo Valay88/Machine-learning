ef4b123d6e3f7e7ba84fb1fd23e1a4a5ad20dd00
Valay Panchal <valay.pan93@gmail.com>
2025-05-28 17:05:54 +0530

    using google word2vec little modification

--- word2vec.py
+++ word2vec.py
@@ -7,11 +7,19 @@ vec_king = wv['king']
 shape = vec_king.shape
 print('shape of the king vector is:',shape)
 print('showing 300 arrays of king:\n',vec_king)
+
+# getting vector array of teaching
 vec_teaching = wv['teaching']
+
+# showing most simillar words of teaching
 simmilar_word = wv.most_similar('teaching')
 print('most simillar word of teaching is:\n',simmilar_word)
+
+## finding simmilarity between hockey and cricket
 simillarity = wv.similarity('hockey','cricket')
 print('similarity between hockey & cricket is:\n',simillarity)
+
+#vectoring king - man + woman the output showing Queen 
 vec = wv['king']-wv['man']+wv['woman']
 similar_king=wv.most_similar([vec])
 print('the output of the vec is:\n',similar_king)
